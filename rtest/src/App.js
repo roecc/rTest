bee4f32c937d4e816353aeadef083f9f9844dbcd
lee <admin@lees-MBP.fritz.box>
2022-05-02 18:44:31 +0200

    added box-shadow vignette

--- rtest/src/App.js
+++ rtest/src/App.js
@@ -6,23 +6,30 @@ const DEFAULT_LATITUDE = 48;
 
 function App() {
   return (
-    <MapContainer center={[52.52, 13.411]} zoom={12} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
-        url="https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png"
-      />
+    <div id="site-container">
+      <div class="box-shadow">
 
-      <Marker position={[52.51, 13.411]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-      <Marker position={[52.52, 13.421]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+      </div>
+      <MapContainer center={[52.52, 13.411]} zoom={12} scrollWheelZoom={true} zoomControl={false}>
+        <TileLayer
+          attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+          url="https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png"
+        />
+
+        <Marker position={[52.51, 13.411]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        <Marker position={[52.52, 13.421]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+
+    
   );
 }
 
