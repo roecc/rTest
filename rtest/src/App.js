707263f3d6d68c440e866596391ac93e7d617dc3
lee <admin@lees-MBP.fritz.box>
2022-05-02 18:20:29 +0200

    toner light

--- rtest/src/App.js
+++ rtest/src/App.js
@@ -6,13 +6,18 @@ const DEFAULT_LATITUDE = 48;
 
 function App() {
   return (
-    <MapContainer center={[52.518, 13.433]} zoom={12} scrollWheelZoom={true}>
+    <MapContainer center={[52.52, 13.411]} zoom={12} scrollWheelZoom={true}>
       <TileLayer
         attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
         url="https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png"
       />
 
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[52.51, 13.411]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+      <Marker position={[52.52, 13.421]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
