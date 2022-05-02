adcaf5e7266fdbbf554a10c3b9f3b885bfc4af72
lee <admin@lees-MBP.fritz.box>
2022-05-02 15:44:51 +0200

    added map

--- rtest/src/App.js
+++ rtest/src/App.js
@@ -1,29 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+
+const DEFAULT_LANGITUDE = -123;
+const DEFAULT_LATITUDE = 48;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>hello world!</code> and save to reload.
-        </p>
-        <p>
-          <code>
-            ISNT THIS NICE?!
-          </code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer center={[52.518, 13.433]} zoom={12} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+        url="https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png"
+      />
+
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
